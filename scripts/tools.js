a8e53695ed1f2884b2ce2434efa3f72ec93c918f
Abhishek Singh <81193958+asingh2023@users.noreply.github.com>
2023-02-05 12:54:20 -0500

    commint

--- scripts/tools.js
+++ scripts/tools.js
@@ -26,7 +26,8 @@ const errorMessage = (type , message) => {
 };
   
 const printMessage = (message) => { 
-  console.log(`\x1b[32m \x1b[1m ${message} \x1b[0m`  );
+  //console.log(`\x1b[32m \x1b[1m ${message} \x1b[0m`  );
+  console.log(message);
 };
 
  
