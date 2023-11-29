8e1e4b9cfda9ed58708f898a44e5395d1accaeae
Minh Pham <f62q1zz>
2023-11-29 11:03:19 -0800

    PORT-8590: Archive script for updating tenant workflow files

--- tenant-config-scripts/README.md
+++ tenant-config-scripts/README.md
@@ -20,7 +20,7 @@ Script to parse through an organization's repositories and mass configure branch
 - `ruleset`: Configs for the Ruleset being applied, check [Ruleset Reference](https://docs.github.com/en/rest/repos/rules?apiVersion=2022-11-28#create-a-repository-ruleset) for more details
 - `headers`: Default headers as provided by Github API
 
-- For testing, replace `repositories` variable with a list with a single tenant before the for-loop or modify the for-loop itself:
+For testing, replace `repositories` variable with a list with a single tenant before the for-loop or modify the for-loop itself:
    - ```Python 
      repositories = ["Test-repo"]
      ```
@@ -55,7 +55,7 @@ Script to parse through an organization's repositories and mass lock branches (s
    - Settings are configured to our current standard rules. Once Rulesets are enabled they can be set to `null` and `false` to let Ruleset handle parsing.
 - `headers`: Default headers as provided by Github API
 
-- For testing, replace `repositories` variable with a list with a single tenant before the for-loop or modify the for-loop itself:
+For testing, replace `repositories` variable with a list with a single tenant before the for-loop or modify the for-loop itself:
    - ```Python 
      repositories = ["Test-repo"]
      ```
@@ -70,3 +70,41 @@ python3 tenant-config-scripts/branchLock.py
 ```
 
 A commented out curl command is also placed at the bottom of the script for reference.
+
+## fixTenantFiles.py
+
+Script to parse through an organization's repositories and create/delete various files for a certain branch. Primarily used to update a certain branch (such as `preview`) to use new workflow files.
+
+- Utilizes `python3` so make sure you have it installed
+- Will require a personal access token with proper organization/project read-write permissions to modify repo (and workflow if changing those yaml files)
+
+### Variables
+
+- `token`: Github access token - variable `<Github_Access_Token>` will need to be replaced with proper data
+   - Should be saved in Github secret if we want to utilize pipeline
+   - If running locally, you will need to generate your own access token with proper permission
+- `organization`: Name for Github organization/project owner you'd like to make changes for
+- `base_branch`: Branch name you'd like to change. Could be change to a list as needed and add a for-loop.
+- `feature_branch`: Name of your feature branch.
+   - If multiple branches are being changed, similarly make a list or generate this variable in each for-loop iteration of the `base_branches` list.
+   - Make sure that it's not a common name that the tenant might already use (such as `preview-validator-fix`)
+- `file_path`: File path for a file being added/edited.
+- `file_content`: File content to overwrite at the `file_path`. Does not append, simply overwrite.
+- `commit_message`: Commit message for creating/editing file.
+- `delete_files`: List of files to be deleted (if they exist)
+- `pull_request_title`
+- `pull_request_body`: Description
+
+For testing, replace `repositories` variable with a list with a single tenant before the for-loop or modify the for-loop itself:
+   - ```Python 
+     repositories = ["Test-repo"]
+     ```
+   - ```Python 
+     for repo in ["Test-repo"]:
+     ```
+
+### Usage
+
+``` Bash
+python3 tenant-config-scripts/fixTenantFiles.py
+```
---
+++ tenant-config-scripts/fixTenantFiles.py
@@ -0,0 +1,171 @@
+import requests, base64
+
+# token = "<Github_Access_Token>"
+token = "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"
+
+headers = {
+    "Authorization": f"token {token}",
+    "Accept": "application/vnd.github.v3+json"
+}
+
+def create_branch(organization:str, repository:str, base_branch:str, new_branch:str):
+    base_url = f"https://api.github.com/repos/{organization}/{repository}/git/refs/heads/{base_branch}"
+    
+    # Get the latest commit SHA of the base branch
+    response = requests.get(base_url, headers=headers)
+    response_json = response.json()
+    latest_sha = response_json['object']['sha']
+
+    # Create a new branch pointing to the latest commit of the base branch
+    new_branch_url = f"https://api.github.com/repos/{organization}/{repository}/git/refs"
+    new_branch_data = {
+        "ref": f"refs/heads/{new_branch}",
+        "sha": latest_sha
+    }
+    response = requests.post(new_branch_url, headers=headers, json=new_branch_data)
+    # print(response.json())
+    print(f"Branch {new_branch} created from {base_branch}")
+
+def commit_and_push_file(organization:str, repository:str, branch:str, file_content:str, file_path:str, commit_message:str):
+    base_url = f"https://api.github.com/repos/{organization}/{repository}/contents/{file_path}"
+
+    # Check if validator file already exists
+    response = requests.get(base_url, headers=headers)
+    if response.status_code == 200:
+        # File exists, get the current content
+        response_json = response.json()
+        current_sha = response_json['sha']
+    elif response.status_code == 404:
+        # File doesn't exist, set the current SHA to None
+        current_sha = None
+    else:
+        # Handle other status codes as needed
+        print(f"Error: {response.status_code}")
+        return
+    
+    file_content_base64 = base64.b64encode(file_content.encode('utf-8')).decode('ascii')
+
+    # Commit the changes
+    commit_url = f"{base_url}?ref={branch}"
+    commit_data = {
+        "message": commit_message,
+        "content": file_content_base64,
+        "sha": current_sha,
+        "branch": branch
+    }
+
+    response = requests.put(commit_url, headers=headers, json=commit_data)
+
+    # Print the result
+    # print(response.json())
+    print(f"File {file_path} created/edited")
+
+def delete_file(organization:str, repository:str, branch:str, file_path:str):
+    delete_url = f"https://api.github.com/repos/{organization}/{repository}/contents/{file_path}?ref={branch}"
+    delete_response = requests.get(delete_url, headers=headers)
+    
+    if delete_response.status_code == 200:
+        # File to be deleted exists, get its current content
+        delete_response_json = delete_response.json()
+        delete_sha = delete_response_json['sha']
+        
+        # Delete the file
+        delete_data = {
+            "message": f"Delete file: {file_path}",
+            "sha": delete_sha,
+            "branch": branch
+        }
+        delete_response = requests.delete(delete_url, headers=headers, json=delete_data)
+        # print(delete_response.json())
+        print(f"File {file_path} deleted")
+    else:
+        print(f"File {file_path} not found")
+
+def create_pull_request(username, repository, base_branch, head_branch, title, body):
+    base_url = f"https://api.github.com/repos/{username}/{repository}/pulls"
+
+    # Create a pull request
+    pull_request_data = {
+        "title": title,
+        "body": body,
+        "head": head_branch,
+        "base": base_branch
+    }
+
+    response = requests.post(base_url, headers=headers, json=pull_request_data)
+    # print(response.json())
+    print(f"PR Created at {response.json()['url']}")
+
+def get_organization_repositories(organization):
+    headers = {
+        "Authorization": f"token {token}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+
+    url = f"https://api.github.com/orgs/{organization}/repos"
+    response = requests.get(url, params={"per_page": 100}, headers=headers)
+
+    # Process the response
+    if response.status_code == 200:
+        repositories = [repo['name'] for repo in response.json()]
+        print(repositories)
+        return repositories
+    else:
+        print("Failed to fetch repositories from the project.")
+        exit()
+
+if __name__ == "__main__":
+    organization = "Fiserv"
+    base_branch = "preview"
+    feature_branch = "devstudio-preview-validator-workflow-update"
+    create_file_path = ".github/workflows/validator.yaml"
+    create_file_content = '''name: Studio Validator
+on:
+  # Triggers the workflow on push or pull request events for the dev, stage, main, preview, and previous branch.
+  push:
+    branches: [ develop,stage,main,preview,previous ]
+  pull_request:
+    branches: [ develop,stage,main,preview,previous ]
+
+jobs:
+  validator:
+    uses: Fiserv/remote-actions/.github/workflows/validator-service.yaml@main
+    secrets: inherit
+'''
+    commit_message = "Fix validator workflow for preview branch"
+    delete_files = [".github/workflows/api-validator.yaml", ".github/workflows/zip-generator.yaml"]
+    pull_request_title = "Preview Branch Validator Update"
+    pull_request_body = "Updating the workflow logic of the preview branch. Please make sure to include same settings in future if pushing from other branches."
+
+    # Get the repositories in the organization
+    repositories = get_organization_repositories(organization)
+
+    for repo in repositories:
+        print(f"Processing repository: {repo}")
+
+        branch_check_url = f"https://api.github.com/repos/{organization}/{repo}/branches/{base_branch}"
+        branch_check_response = requests.get(branch_check_url, headers=headers)
+
+        if branch_check_response.status_code == 200:
+            print(f"Branch '{base_branch}' exists.")
+        elif branch_check_response.status_code == 404:
+            print(f"Branch '{base_branch}' does not exist.\n")
+            continue
+        else:
+            print(f"Error: {branch_check_response.status_code}")
+            print(branch_check_response.text)
+            continue
+
+        # Create a feature branch
+        create_branch(organization, repo, base_branch, feature_branch)
+
+        # Commit and push changes to the feature branch
+        commit_and_push_file(organization, repo, feature_branch, create_file_path, create_file_path, commit_message)
+
+        # Delete old validator files
+        for old_file in delete_files:
+            delete_file(organization, repo, feature_branch, old_file)
+
+        # Create a pull request
+        create_pull_request(organization, repo, base_branch, feature_branch, pull_request_title, pull_request_body)
+        print()
